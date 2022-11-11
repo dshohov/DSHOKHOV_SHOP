d58681f9db651b26f0bd07032cc131e8cdc7d22f
Dmitry Shokhov <dshohov@gmail.com>
2022-11-11 23:31:30 +0300

    Add models

---
+++ shokhov_shop/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using shokhov_shop.Data.Enum;
+
+namespace shokhov_shop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public People People { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Sub_Catedory> SubCatedories { get; set; }
+    }
+}
